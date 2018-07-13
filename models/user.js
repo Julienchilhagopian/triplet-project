e8271e23fee73b84581c33018fda00cc33133cb1
danielakujawa <kujawa.daniela@gmail.com>
2018-07-13 14:45:06 +0200

    databse connected

--- models/user.js
+++ models/user.js
@@ -1,1 +1,42 @@
 'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  categories: {
+    type: String,
+    required: true,
+    enum: ['Medicines', 'Food', 'Education']
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  mail: {
+    type: String,
+    required: true
+  },
+  socialMedia: {
+    type: String
+  },
+  website: {
+    type: String
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
