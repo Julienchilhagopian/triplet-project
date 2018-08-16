c2ec323b42f5f7c8810fb5787138e5efb90e6201
danielakujawa <kujawa.daniela@gmail.com>
2018-08-16 13:20:07 +0200

    activate route

--- routes/activate.js
+++ routes/activate.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
 const User = require('../models/user');
 
